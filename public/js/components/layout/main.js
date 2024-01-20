e5f78a79d4e789ca2238c2a0b178d5c48d9e81bb
Cheikh Ndiaye <khechbrain@gmail.com>
2024-01-20 00:00:01 +0000

    Update des components

--- public/js/components/layout/main.js
+++ public/js/components/layout/main.js
@@ -7,9 +7,7 @@ export default class Header extends HTMLElement {
     }
 
     connectedCallback() {
-        // console.log(this)
         this.render()
-        // this._style()
     }
 
     disconnectedCallback() {
@@ -50,37 +48,6 @@ export default class Header extends HTMLElement {
         }
         
         `
-    }
-
-
-    _style() {
-        const style = document.createElement('style')
-        style.textContent = `
-        ${this.tagName} .main-header{
-            padding: 0;
-            margin: 0;
-            display: flex;
-            justify-content: space-between;
-            padding: 20px;
-        }
-        ${this.tagName} .main-header>.menu-a{
-            padding: 0;
-            background-color: #002ea3;
-            width: 150px;
-            height: 37px;
-            border-radius: 23px;
-            justify-content: center;
-            align-items: center;
-            font-weight: 600;
-        }
-        ${this.tagName} .main-header .join {
-
-        }
-
-        `
-        console.log(this.header)
-        this.appendChild(style)
-
     }
 
     get header() {
