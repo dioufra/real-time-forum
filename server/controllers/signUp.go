04842d37f51f5f53fb51b5bdc56003eeb1a32f7e
Cheikh Ndiaye <khechbrain@gmail.com>
2024-01-21 11:11:57 +0000

    Ajout des requetes SQL

--- server/controllers/signUp.go
+++ server/controllers/signUp.go
@@ -4,29 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"real-time-forum/server/config"
 	"real-time-forum/server/models"
 )
 
 // the path will be validated using middlewares.
 
-func SignUp(res http.ResponseWriter, req *http.Request) {
+func SignUp(w http.ResponseWriter, r *http.Request) {
 	userData := models.UserData{}
-	if req.Method == http.MethodPost {
+	if r.Method == http.MethodPost {
 		var newUser models.User
-		decoder := json.NewDecoder(req.Body)
+		decoder := json.NewDecoder(r.Body)
 		if err := decoder.Decode(&newUser); err != nil {
 			fmt.Println(err)
-			http.Error(res, "Invalid request payload", http.StatusBadRequest)
+			http.Error(w, "Invalid request payload", http.StatusBadRequest)
 			return
 		}
-		defer req.Body.Close()
-		userData.User = newUser
+		defer r.Body.Close()
 		fmt.Println(newUser)
 
-		res.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(res).Encode(userData)
+		// Insert the new user into the database
+		db, err := config.GetDB()
+		if err != nil {
+			fmt.Println("connection database Error", err)
+			os.Exit(0)
+		}
+		insertQuery := "INSERT INTO users (username, password) VALUES (?, ?)"
+		result, err := db.Exec(insertQuery, newUser.Username, newUser.Password)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		// Get the ID of the newly inserted user
+		userID, _ := result.LastInsertId()
+
+		newUser.Id = int(userID)
+		userData.User = newUser
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(userData)
 	} else {
-		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 }
