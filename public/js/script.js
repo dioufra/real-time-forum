4ef92a3ed4b5c14a3b78ab4a730af2e2fbc7b47f
frdiouf <fanfandiouf2016@gmail.com>
2024-01-24 07:34:00 +0000

    add posts + pagination components

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,14 @@
 let components = [
-    {tagName:'c-header',src:"../js/components/layout/header.js"},
-    {tagName:'c-main',src:"../js/components/layout/main.js"},
-    {tagName:'c-login',src:"../js/components/login.js"},
-    {tagName:'c-register',src:"../js/components/register.js"},
+    {tagName:'c-header',src:"./components/layout/header.js"},
+    {tagName:'c-main',src:"./components/layout/main.js"},
+    {tagName:'c-login',src:"./components/login.js"},
+    {tagName:'c-register',src:"./components/register.js"},
+    {tagName:'sc-user-info',src:"./components/userInfo.js"},
+    {tagName:'c-posts-container',src:"./components/postsContainer.js"},
+    {tagName:'c-post',src:"./components/post.js"},
+    {tagName:'c-pagination',src:"./components/pagination.js"},
 ]
+
 // Define the custom web component
 components.forEach(component => {
     import(component.src).then(module => {
