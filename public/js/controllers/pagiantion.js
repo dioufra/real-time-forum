0ceadbb924c877d9e6658a87e476edf4e1479a9b
frdiouf <fanfandiouf2016@gmail.com>
2024-03-02 16:30:14 +0000

    scroll logic ongoing

--- public/js/controllers/pagiantion.js
+++ public/js/controllers/pagiantion.js
@@ -8,7 +8,6 @@ class PaginationController {
         this.isLoading = true
     }
     setCurrentPage(page){
-        console.log(page);
         this.CurrentPage = page
         POST_CONTROLLER.filteredPosts = POST_CONTROLLER.posts.filter((post,index)=> {
             return index >= (page - 1) * this.PageSize && index < (page *  this.PageSize)
